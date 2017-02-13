d2944002f3b69ee57f7594119ccd357101e33c35
Mark Sater <markjsater@gmail.com>
2017-02-12 22:16:45 -0700

    initial commit

---
+++ LCD_orignal/LCD_orignal.ino
@@ -0,0 +1,89 @@
+#include <Wire.h>
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(4,6,10,11,12,13); 
+
+const int toneButton = 3;
+const int buttonUp = 7;
+const int buttonDown = 8;
+const int increment = 1;
+int frequencyTarget=200;
+const int maxFrequency=299;
+const int minFrequency=33;
+
+
+
+void setup()
+{
+  Wire.begin(8);
+  Serial.begin(9600);
+  lcd.begin (16,2);
+  pinMode(buttonUp,INPUT);
+  pinMode(buttonDown,INPUT);
+  lcd.setCursor (0,0);
+  lcd.print ("Selector: ");
+  lcd.setCursor (10,0);
+  lcd.print (frequencyTarget);
+  
+
+}
+
+void loop() {
+  Serial.begin(9600);
+  lcd.setCursor(0,1);
+  lcd.print("Frequency: ");
+  lcd.setCursor(11,1);
+  Wire.onReceive(receiveEvent);
+delay(150);
+  
+lcd.setCursor (10,0);
+if(digitalRead (buttonUp) == HIGH && digitalRead(buttonDown) == LOW )
+  {
+    //tone(9, frequencyTarget);
+    if(frequencyTarget <= maxFrequency)
+    {
+    frequencyUp();
+    }
+  }
+ if(digitalRead (buttonDown) == HIGH && digitalRead(buttonUp) == LOW )
+  {
+    //tone(9, frequencyTarget);
+    if(frequencyTarget >= minFrequency)
+    {
+    frequencyDown();
+    }
+  }
+ if(frequencyTarget < 100)
+  {
+    lcd.setCursor(12,0);
+    lcd.print(" ");
+  }
+ if(digitalRead(toneButton) == HIGH)
+  {
+    tone(9, frequencyTarget);
+  }
+ if(digitalRead(toneButton) == LOW)
+  {
+    noTone(9);
+  }
+}
+void frequencyUp()
+{
+    frequencyTarget = frequencyTarget + increment;
+    lcd.print(frequencyTarget);  
+   // delay(150);
+}
+
+void frequencyDown()
+{
+    frequencyTarget = frequencyTarget - increment;
+    lcd.print(frequencyTarget);
+    //delay(150);
+}
+
+void receiveEvent(int bytes){
+  int x = Wire.read();
+  lcd.print(x);
+  Serial.print(x);
+}
+ 
+
---
+++ Microphone_DFT-_original/Microphone_DFT-_original.ino
@@ -0,0 +1,103 @@
+#include <Wire.h>
+
+const int button = 3;
+const int mic = A0;
+const float pi = 3.141592654;
+const float N = 600.0; //Number of samples
+float T; // k is divided by delta times number of samples or divided by t (Time)
+float f[600];  //Signal array
+float xSum, ySum, delta;
+signed long startTime, endTime, elTime;
+float Fx[300], Fy[300]; // Real and imaginary components of transform
+float Mag[300], Freg[300];
+unsigned int k,n,r,i ;
+boolean dftProcess = false;
+boolean recording = false;
+
+void setup() 
+{
+  
+  Wire.begin();
+  Serial.begin(9600);
+}
+
+void loop() 
+{
+  int buttonPush = digitalRead(button);
+  
+  if(buttonPush == HIGH && dftProcess == false && recording == false){
+    Record();
+  }
+  Serial.println(buttonPush);
+  if(dftProcess == true && recording == false){
+    byte Transmission=0;  
+    for(k=0; k <= N/2-1; k++){
+      xSum=0; ySum =0;
+      for(n=0;n<=N-1;n++){
+        float arg = 2*pi*k*n/N;
+        xSum = xSum + f[n]*cos(arg);
+        ySum = ySum + f[n]*sin(arg);
+      }
+      Fx[k]=(1.0/N)*xSum;
+      Fy[k]=(1.0/N)*(-ySum);
+      Mag[k]=sqrt( pow(Fx[k],2)+pow(Fy[k],2) );
+      Freg[k]=float(k)/T;// k/(T)or you can use k/(N*delta)
+    }
+    Transmission = GetMaxFrequency();
+    Wire.beginTransmission(8);
+    Wire.write(Transmission);
+    Wire.endTransmission();
+    dftProcess = false;
+   
+  }
+}
+
+
+float GetMaxFrequency()
+{
+  float maxFreq = 0;
+  
+  for (int i = 0; i <= N-1; i++){
+    maxFreq = max(Mag[i],maxFreq);
+  }
+  
+  for (int i = 0; i <=N-1;i++){
+    if (Mag[i] == maxFreq){
+      return Freg[i];
+    }
+  }
+}
+
+void Record()
+{
+  
+  recording = true;
+  Microphone();
+  recording = false;
+  dftProcess = true;
+}
+
+void Microphone()
+{
+  float sum = 0;
+  float Average;
+  
+  startTime = millis();
+  for(r=0;r<=N-1;r++){
+    f[r]=analogRead(mic)*(5.0/1023.0);
+    sum=sum+f[r];
+    delay(2);
+  }
+  endTime = millis();
+  elTime = endTime - startTime;
+  delta = (elTime / N)/1000.0;
+  T = N*delta; // changed to N instead of N-1
+  Average=sum/N;
+  for(r=0;r<=N-1;r++){
+    f[r]=500*(f[r]-Average);
+    Serial.println(f[r]);
+  }
+  Serial.println("================================================="); 
+}
+
+
